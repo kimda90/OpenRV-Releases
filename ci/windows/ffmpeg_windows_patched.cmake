311aa679bacab3f2f9f802ce38ef6b485ef370e5
Daniel Kim <kimda90@gmail.com>
2026-02-10 13:59:31 -0300

    OpenRV passes --extra-ldflags=-LIBPATH:<openssl-lib-dir> into FFmpeg.

--- ci/windows/ffmpeg_windows_patched.cmake
+++ ci/windows/ffmpeg_windows_patched.cmake
@@ -246,6 +246,13 @@ LIST(APPEND RV_FFMPEG_COMMON_CONFIG_OPTIONS "--disable-vaapi")
 LIST(APPEND RV_FFMPEG_COMMON_CONFIG_OPTIONS "--disable-doc")
 IF(RV_TARGET_WINDOWS)
   LIST(APPEND RV_FFMPEG_COMMON_CONFIG_OPTIONS "--toolchain=msvc")
+  # OpenSSL 3.x on Windows requires additional system libs for link checks.
+  # Without these, FFmpeg's configure may fail with "openssl not found".
+  LIST(APPEND RV_FFMPEG_COMMON_CONFIG_OPTIONS "--extra-libs=-lws2_32")
+  LIST(APPEND RV_FFMPEG_COMMON_CONFIG_OPTIONS "--extra-libs=-lgdi32")
+  LIST(APPEND RV_FFMPEG_COMMON_CONFIG_OPTIONS "--extra-libs=-ladvapi32")
+  LIST(APPEND RV_FFMPEG_COMMON_CONFIG_OPTIONS "--extra-libs=-lcrypt32")
+  LIST(APPEND RV_FFMPEG_COMMON_CONFIG_OPTIONS "--extra-libs=-luser32")
 ENDIF()
 
 # Change the condition to TRUE to be able to debug into FFmpeg.
@@ -355,6 +362,18 @@ LIST(REMOVE_DUPLICATES RV_FFMPEG_CONFIG_OPTIONS)
 LIST(REMOVE_DUPLICATES RV_FFMPEG_EXTRA_C_OPTIONS)
 LIST(REMOVE_DUPLICATES RV_FFMPEG_EXTRA_LIBPATH_OPTIONS)
 LIST(REMOVE_DUPLICATES RV_FFMPEG_EXTERNAL_LIBS)
+
+# On Windows, OpenSSL's dependency file passes `--extra-ldflags=-LIBPATH:<dir>`.
+# FFmpeg's MSVC flag filter treats `-L<dir>` specially and rewrites it to `-libpath:<dir>`,
+# but `-LIBPATH:<dir>` gets mangled into `-libpath:IBPATH:<dir>` and breaks library discovery.
+IF(RV_TARGET_WINDOWS)
+  SET(_rv_ffmpeg_extra_libpath_options_sanitized "")
+  FOREACH(_rv_ffmpeg_opt IN LISTS RV_FFMPEG_EXTRA_LIBPATH_OPTIONS)
+    STRING(REPLACE "--extra-ldflags=-LIBPATH:" "--extra-ldflags=-L" _rv_ffmpeg_opt "${_rv_ffmpeg_opt}")
+    LIST(APPEND _rv_ffmpeg_extra_libpath_options_sanitized "${_rv_ffmpeg_opt}")
+  ENDFOREACH()
+  SET(RV_FFMPEG_EXTRA_LIBPATH_OPTIONS "${_rv_ffmpeg_extra_libpath_options_sanitized}")
+ENDIF()
 
 SET(_ffmpeg_preprocess_pkg_config_path
     $ENV{PKG_CONFIG_PATH}
